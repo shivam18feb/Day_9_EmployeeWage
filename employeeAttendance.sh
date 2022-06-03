25afc8620dfcd874309823ce74ebd37921db470f
AshokKoda <a.koda59@gmail.com>
2022-06-03 08:40:15 +0530

    Refactor:calculate employee daily wage

--- employeeAttendance.sh
+++ employeeAttendance.sh
@@ -1,3 +1,8 @@
+FULL_DAY_WORKING=8
+WAGE_PER_HR=20
+
+dailyWage=0
+
 attendance=$((RANDOM%2))
 echo "Random number: $attendance"
 
@@ -6,4 +11,6 @@ then
 	echo "Employee is absent"
 else
 	echo "Employee is present"
+	dailyWage=$(($FULL_DAY_WORKING*$WAGE_PER_HR))
+	echo "Daily wage: $dailyWage"
 fi
