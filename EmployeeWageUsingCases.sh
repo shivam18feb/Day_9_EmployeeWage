3f9e6d92497e7e3967e45c12800e0d94887ee1d4
AshokKoda <a.koda59@gmail.com>
2022-06-03 09:04:06 +0530

    Refactor:Calculate employee monthly wage

--- EmployeeWageUsingCases.sh
+++ EmployeeWageUsingCases.sh
@@ -1,9 +1,11 @@
 WAGE_PER_HR=20
 FULL_DAY_WORKING_HR=8
 HALF_DAY_WORKING_HR=4
-
+MAX_WORKING_DAYS=20
 dailyWage=0
 
+for (( i=0;i<$MAX_WORKING_DAYS;i++ ))
+do
 attendance=$((RANDOM%3))
 case $attendance in
 	1)
@@ -23,3 +25,4 @@ case $attendance in
 		dailyWage=0
 		echo "Daily wage: $dailyWage"
 esac
+done
